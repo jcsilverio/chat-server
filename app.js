415f410bb6fc6380ea854731e7d5a3b200ef88e5
JC Silverio <silverio@impulsevfx.com>
2017-12-10 02:15:35 -0500

    data persists with redis

--- app.js
+++ app.js
@@ -2,37 +2,48 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var redis = require('redis');
+var redisClient = redis.createClient();
 
 
-var storedMessages = [];
-
-var saveMessage = function (name, data){
-  storedMessages.push({
+var saveMessage = function(name, data) {
+  //need to turn obj into string to store in redis
+  var message = JSON.stringify({
     name: name,
     data: data
   });
 
-  // if more than 10 messges long, remove the first message
-  if(storedMessages.length > 10) {
-    storedMessages.shift();
-  }
-  console.log(storedMessages);
+  redisClient.lpush("messageList", message, function(err, response) {
+    console.log('lpush err:', err);
+    //keep newest 10 messages in the array
+
+    redisClient.ltrim("messageList", 0, 9);
+  });
 }
 
 
 io.on('connection', function(client) {
   client.on('join', function(name) {
-    // emit a message on the connecting client for each existing message
-    storedMessages.forEach(function(message){
-      client.emit('messages', message.name + ": " + message.data);
 
+    // emit a message on the connecting client for each existing message
+    redisClient.lrange("messageList", 0, -1, function(err, messages) {
+    console.log('lrange err:', err);
+      //reverse messages to be read in correct order
+      messages = messages.reverse();
+
+      messages.forEach(function(message) {
+        //parse into JSON object
+        message = JSON.parse(message);
+        client.emit("messages", message.name + ": " + message.data);
+      });
     });
     client.nickname = name;
+
     console.log(`${name} connected...`);
   });
 
 
-client.on('messages', function(data) { //listen for message events\
+  client.on('messages', function(data) { //listen for message events\
     var nickname = client.nickname;
     client.broadcast.emit('messages', nickname + ": " + data);
 
